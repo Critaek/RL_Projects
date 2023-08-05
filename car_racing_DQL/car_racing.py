a9b8942bad34f5cb6665da9a3409b88f97e442d4
Cristiano <serra.cri2000@gmail.com>
2023-08-05 12:30:13 +0200

    Commit

--- car_racing_DQL/car_racing.py
+++ car_racing_DQL/car_racing.py
@@ -6,13 +6,13 @@ if __name__ == '__main__':
     env = gym.make("CarRacing-v2", continuous=False)
 
     best_score = -np.inf
-    load_checkpoint = False
-    n_games = 250
+    load_checkpoint = True
+    n_games = 1000
     force_train = True
 
     agent = DQNAgent(gamma=0.99, epsilon=0.1, lr=0.0001,
                      input_dims=(env.observation_space.shape),
-                     n_actions=5, mem_size=50000, eps_min=0.1,
+                     n_actions=5, mem_size=50000, eps_min=0.01,
                      batch_size=32, replace=1000, eps_dec=1e-5,
                      chkpt_dir='models/', algo='DQNAgent',
                      env_name='CarRacing-v2')
@@ -53,7 +53,7 @@ if __name__ == '__main__':
             'epsilon %.2f' % agent.epsilon, 'steps', n_steps)
 
         if avg_score > best_score:
-            if not load_checkpoint:
+            if not load_checkpoint or force_train:
                 agent.save_models()
             best_score = avg_score
 
